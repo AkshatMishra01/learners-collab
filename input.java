45929a1a84850b4e1cd58c658dcea1ddbbaf279b
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 13:20:36 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+import java.math.BigDecimal;
 import java.util.Scanner;
 public class Main
 {
@@ -11,6 +13,10 @@ System.out.println("The number entered by the user is " + data);
 System.out.println("Enter the string");
 String name = input.next();
 System.out.println("The entered string is " + name);
+BigInteger value = input.nextBigInteger();
+System.out.println("The entered biginteger is" + value);
+BigDecimal number = input.nextBigDecimal();
+System.out.println("The entered bigdecimal is " + number);
 input.close();
 }
 }
