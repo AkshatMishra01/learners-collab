91b153c086a04d6905bbe106b1ef2ddc11a9bf6f
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 11:52:12 +0530

    Create output.java

---
+++ output.java
@@ -0,0 +1,9 @@
+public class output
+{
+int number1 = 234;
+double number2 = 56.7 ;
+// number 1 having integer value
+System.out.println(number1);
+// number 2 having decimal value
+System.out.println(number2);
+}
