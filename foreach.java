edd643cae84bc23898823ec0990dbdb395b82e90
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 15:29:54 +0530

    Create foreach.java

---
+++ foreach.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class foreach
+{
+public static void main()
+{
+int[] data = new int[20];
+Scanner input = new Scanner(System.in);
+for(int datas: data)
+{
+int data = input.nextInt();
+}
+System.out.println(
+}
+}
