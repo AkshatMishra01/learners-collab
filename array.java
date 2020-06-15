15c30068588eb7971a5410dca33b94a30e471e3e
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 11:45:18 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,21 @@
+import java.util.scanner;
+
+public class array
+{
+public static void main(String[] arrayargs)
+{
+System.out.println("This is the array programme in java");
+int[] array;
+array= new int[1000];
+int i=0;
+System.out.println(array[5]);
+System.out.println(array[1]);
+System.out.println(array[2]);
+System.out.println(array[7]);
+for(int arrays: array)
+{
+System.out.println("The element at index" i++ "are" arrays);
+}
+
+}
+}
