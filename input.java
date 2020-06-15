5dddc83beabe74420cad2982fdb4709424e5bf5f
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 12:06:25 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+public class input
+{
+public static void main(String[] inputargs)
+{
+System.out.println("Input from the user");
+//declare the object for the scanner class
+Scanner read = new Scanner(System.in);
+int data = read.nextint();
+System.out.println("The number entered by the user is" + data);
+String name = read.next();
+System.out.println("The entered string is" + name);
+}
+}
