82bc7c92f088ee6995242e5a9c521f8becaddba0
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-15 12:17:58 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class input
+public class Main
 {
 public static void main(String[] inputargs)
 {
 System.out.println("Input from the user");
 //declare the object for the scanner class
-Scanner read = new Scanner(System.in);
-int data = read.nextint();
-System.out.println("The number entered by the user is" + data);
-String name = read.next();
-System.out.println("The entered string is" + name);
+Scanner input = new Scanner(System.in);
+int data = input.nextInt();
+System.out.println("The number entered by the user is " + data);
+System.out.println("Enter the string");
+String name = input.next();
+System.out.println("The entered string is " + name);
+input.close();
 }
- read.close();
 }
