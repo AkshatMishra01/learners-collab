9fad06b03137d3caa19ad07b1ff672250409d7f3
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-16 08:33:58 +0530

    if else workflow in java

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ifelse
+{
+public static void main()
+{
+Scanner input = new Scanner(System.in);
+int data = input.nextInt();
+if(data > 0)
+{
+System.out.println("The entered integer" +data "is positive");
+}
+else(data < 0)
+{
+System.out.println("The entered integer" +data "is positive");
+}
+input.close();
+}
+}
