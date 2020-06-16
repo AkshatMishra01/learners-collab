1df584b55854097ce76ee9871ac60e0855c90810
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-16 09:29:38 +0530

    Create infinite.java

---
+++ infinite.java
@@ -0,0 +1,10 @@
+// Infinite for Loop
+
+class Infinite {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 2; --i) {
+            System.out.println("Hello");
+        }
+    }
+}
