6d484d79656a3dc239e63e749cecc3bf5fa83ba0
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-16 09:02:48 +0530

    Switch workflow in java

---
+++ switch.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.math.BigDceimal;
+import java.util.Scanner;
+public class switch
+{
+public static void main(String[] switchargs)
+{
+System.out.print("This is a switch programme");
+Scanner input = new Scanner(System.in);
+BigInteger day = input.nextInt();
+switch(day)
+{
+case 199999999567:
+System.out.println("Hello There! Beautiful");
+break;
+case 543768987541:
+System.out.println("Well, u choose the secnd alternative");
+break;
+}
+input.close();
+}
+}
