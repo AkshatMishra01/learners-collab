ef9af70409174af095fa03da5805364d2ae8de77
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-17 07:48:24 +0530

    How to copy an array in java

---
+++ copyarr.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Main
+{
+public static void main(String[] copyarrgs)
+{
+int [] numbers = {1,2,7,3,4,10}
+int [] integers = numbers;
+for(int digit: integers)
+{
+System.out.println("The copied elements in the second array are:" + digit);
+}
+}
+}
