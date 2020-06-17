fbea4f3d7f94b79331ad455b707f8eafd4282ad2
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-17 08:09:35 +0530

    Update copyarr.java

--- copyarr.java
+++ copyarr.java
@@ -11,3 +11,18 @@ System.out.println("The copied elements in the second array are:" + digit);
 }
 }
 }
+// 2 nd method to cpoy an array in java
+import java.util.Arrays;
+public class Main
+{
+public static void main(String[] copyargs)
+{
+int [] numbers = {9,1,4,,2,3};
+int [] substitue = new int [6];
+for(i=0; i<=sumbers.length ;i++) 
+{
+substitute[i] = numbers[i]
+}
+System.out.println(Array.toString(substitue));
+}
+}
