1d21b765a096919f730bc9aa81844a0e90b8524b
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-06-23 08:36:48 +0530

    not solved pproperly shlok

--- challenge4.cpp
+++ challenge4.cpp
@@ -23,3 +23,6 @@ base *ptr=&obj;
 ptr->function();
 return 0;
 }
+//this is not the approriate solution  my friend, i asked u to add a line of code into the program that makes the main function call 
+//function of the base class. and u added 3 lines of code along with that, when i compiled this code, i found no change in the working code
+//i request u to find the solution at programiz.com for relevance.
