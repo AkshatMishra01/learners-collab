29d2c91ead4358a97de16c627983f854cd73794d
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2020-06-25 08:15:18 +0530

    added public access modifier in offspring
    object of class offspring called the function get _function
    and the display function
    as the function name were matching i changed their name to get_function1 and get_function2 respectively
    and created variables and made them private

--- challenge5.cpp
+++ challenge5.cpp
@@ -2,26 +2,38 @@
 using namespace std;
 class mom
 {
+   private:
+int a;
 public:
-void function()
+void get_function1(int n)
 {
-cout<<"moms features.\n";
+a=n;
 }
 };
 class dad
 {
 public:
-void function()
+void get_function2(int n)
 {
-cout<<"dads features.\n";
+ b=n;  
 }
 };
 class offspring: public mom , public dad
 {
-   cout<<"i am the offspring";
+public:
+void display()
+{
+cout<<"moms features\n"<<endl;
+cout<<"dads features\n"<<endl;
+   cout<<"i am the offspring"<<endl;
+}
 };
 int main()
 {        
 offspring obj;
+   obj.get_function();
+   obj.get_function();
+   obj.display();
+   
 return 0; 
 }
